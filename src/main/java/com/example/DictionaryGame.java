ce8496dae4961cd441d5bd6ebae38b9f2bbd6dcd
PattyPat16120 <patrickquach1@gmail.com>
2024-05-17 21:41:13 -0400

    added event for detecting when players send a message

--- src/main/java/com/example/DictionaryGame.java
+++ src/main/java/com/example/DictionaryGame.java
@@ -2,6 +2,12 @@ package com.example;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.message.v1.ServerMessageEvents;
+import net.minecraft.network.message.MessageType;
+import net.minecraft.network.message.SignedMessage;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,7 +22,16 @@ public class DictionaryGame implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
 		LOGGER.info("Hello Fabric world!");
+		ServerMessageEvents.CHAT_MESSAGE.register(this::onChatMessage);
+	}
+
+	public void onChatMessage(SignedMessage msg, ServerPlayerEntity plr, MessageType.Parameters params) {
+		MinecraftServer s = plr.getServer();
+		s.sendMessage(Text.literal(plr.getDisplayName().getString() + " sent a message!"));
+		if (msg.getContent().getString().equalsIgnoreCase("!begin")) {
+			s.sendMessage(Text.literal("begin the game!!!"));
+			plr.sendMessage(Text.literal("aaaa"));
+		}
 	}
 }
