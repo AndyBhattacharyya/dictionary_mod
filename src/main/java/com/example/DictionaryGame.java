9f1bba76018c50561fab57d7b6a01920ef8eb082
George Zhang <geetransit@gmail.com>
2024-05-17 22:01:49 -0400

    Hide word from chosen player

--- src/main/java/com/example/DictionaryGame.java
+++ src/main/java/com/example/DictionaryGame.java
@@ -16,6 +16,7 @@ public class DictionaryGame implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("dictionarygame");
+	String currentName;
 
 	@Override
 	public void onInitialize() {
@@ -24,14 +25,27 @@ public class DictionaryGame implements ModInitializer {
 		// Proceed with mild caution.
 		LOGGER.info("Hello Fabric world!");
 		ServerMessageEvents.CHAT_MESSAGE.register(this::onChatMessage);
+		ServerMessageEvents.ALLOW_CHAT_MESSAGE.register(this::allowChatMessage);
 	}
 
 	public void onChatMessage(SignedMessage msg, ServerPlayerEntity plr, MessageType.Parameters params) {
 		MinecraftServer s = plr.getServer();
+		currentName = plr.getDisplayName().getString();
 		s.sendMessage(Text.literal(plr.getDisplayName().getString() + " sent a message!"));
 		if (msg.getContent().getString().equalsIgnoreCase("!begin")) {
 			s.sendMessage(Text.literal("begin the game!!!"));
 			plr.sendMessage(Text.literal("aaaa"));
 		}
+	}
+
+	public boolean allowChatMessage(SignedMessage msg, ServerPlayerEntity plr, MessageType.Parameters params) {
+		MinecraftServer s = plr.getServer();
+		String name = plr.getDisplayName().getString();
+		if (currentName == null || !currentName.equals(name))  // let all other messages through
+			return true;
+		String word = msg.getContent().getString();
+		s.sendMessage(Text.literal(name + " picked a string!"));
+		s.sendMessage(Text.literal("the string is:" + word));
+		currentName = null;
 	}
 }
