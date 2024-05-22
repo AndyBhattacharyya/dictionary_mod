bfd4be1dd3839bc32352739daf00ed317ad14446
andybhattacharyya <bhattacharyya.anand@gmail.com>
2024-05-21 22:42:13 -0400

    Andy temp progress

--- src/main/java/com/example/DictionaryGame.java
+++ src/main/java/com/example/DictionaryGame.java
@@ -3,6 +3,7 @@ package com.example;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.message.v1.ServerMessageEvents;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.message.MessageType;
 import net.minecraft.network.message.SignedMessage;
 import net.minecraft.server.MinecraftServer;
@@ -13,7 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 enum GAMESTATE {
-	GAMESTART, GAMEFINISH, GAMEWORD, GAMEEXAMPLE, NOGAME
+	GAMESTART, GAMEEXAMPLE, NOGAME, ONGOING
 }
 
 public class DictionaryGame implements ModInitializer {
@@ -28,48 +29,53 @@ public class DictionaryGame implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 		LOGGER.info("Hello Fabric world!");
-		ServerMessageEvents.CHAT_MESSAGE.register(this::onChatMessage);
 		ServerMessageEvents.ALLOW_CHAT_MESSAGE.register(this::onMsgSent);
 	}
 
-	public void onChatMessage(SignedMessage msg, ServerPlayerEntity plr, MessageType.Parameters params) {
-		MinecraftServer s = plr.getServer();
-		s.sendMessage(Text.literal(plr.getDisplayName().getString() + " sent a message!"));
-		if (msg.getContent().getString().equalsIgnoreCase("!begin")) {
-			s.sendMessage(Text.literal("begin the game!!!"));
-			plr.sendMessage(Text.literal("aaaa"));
-
-		}
-		plr.getServer().getPlayerManager().broadcast(Text.literal("Te"), true);
-	}
-	//static boolean GAMEON = true; <-- Wanted to implement a feature where it prevents multiple occurences of the same game
 
 	GAMESTATE state = GAMESTATE.NOGAME;
-	String game_word = "";
+	String initplayername = null;
+	String gameword = "";
+	String gameexamlpe ="";
+
 	public boolean onMsgSent(SignedMessage msg, ServerPlayerEntity plr, MessageType.Parameters params) {
-		String msgToBeSent = msg.getContent().getString();
+		String message = msg.getContent().getString();
+		switch(state){
+			case NOGAME:
+				if(message.equalsIgnoreCase("!dict")){
+					initplayername = plr.getDisplayName().getString();
+					plr.getServer().getPlayerManager().broadcast(Text.literal("It is now "+initplayername+" turn"), false);
+					plr.sendMessage(Text.literal("Enter your word:"));
+					state = GAMESTATE.GAMESTART;
+					return false;
+				}
+			case GAMESTART:
+				if(plr.getDisplayName().getString().equals(initplayername)){
+					gameword = message;
+					plr.sendMessage(Text.literal("Enter your hint:"));
+					state = GAMESTATE.GAMEEXAMPLE;
+					return false;
+				}
+			case GAMEEXAMPLE:
+				if(plr.getDisplayName().getString().equals(initplayername)) {
+					gameexamlpe = message;
+					plr.getServer().getPlayerManager().broadcast(Text.literal(initplayername + " Hint: " + gameexamlpe), false);
+					return false;
+				}
+			case ONGOING:
+				if(!plr.getDisplayName().getString().equals(initplayername)){
+					if(message.equalsIgnoreCase(gameword)){
+						String tmpname = plr.getDisplayName().getString();
+						plr.getServer().getPlayerManager().broadcast(Text.literal(tmpname+ " Got it Correct: " + gameword), false);
+						plr.getServer().getPlayerManager().broadcast(Text.literal("It is now "+tmpname+" turn"), false);
+						plr.sendMessage(Text.literal("Enter your word:"));
+						state = GAMESTATE.GAMESTART;
+						initplayername = tmpname;
+					}
 
-		if(state.equals(GAMESTATE.GAMEEXAMPLE)){
+				}
 
 		}
-
-		if (state.equals(GAMESTATE.GAMEWORD)){
-			game_word = msg.getContent().getString();
-			state = GAMESTATE.GAMEEXAMPLE;
-			return false;
-		}
-
-		if (msgToBeSent.equalsIgnoreCase("!dict") && !state.equals(GAMESTATE.NOGAME)) {
-			state = GAMESTATE.GAMESTART;
-			plr.sendMessage(Text.literal("Enter your word:"));
-			state = GAMESTATE.GAMEWORD;
-			return true;
-
-		}
-		else{
-			plr.sendMessage(Text.literal("Sorry a game has already started"));
-		}
-
-		return true;
+	return true;
 	}
 }
