86d24ccb900ab39964f1c04d40bfbe1e0dae8402
andybhattacharyya <bhattacharyya.anand@gmail.com>
2024-06-14 20:26:36 -0400

    New Game Template

--- src/main/java/com/example/Events.java
+++ src/main/java/com/example/Events.java
@@ -2,17 +2,10 @@ package com.example;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.fabricmc.fabric.api.message.v1.ServerMessageEvents;
-import net.minecraft.network.message.MessageType;
-import net.minecraft.network.message.SignedMessage;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class DictionaryGame implements ModInitializer {
+public class Events implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
@@ -25,7 +18,7 @@ public class DictionaryGame implements ModInitializer {
 		// Proceed with mild caution.
 		LOGGER.info("Hello Fabric world!");
 //		ServerMessageEvents.CHAT_MESSAGE.register(DictionaryMod_PPQ::onChatMessage);
-		ServerMessageEvents.ALLOW_CHAT_MESSAGE.register(DictionaryMod_PPQ::onMsgSent);
-		ServerTickEvents.START_SERVER_TICK.register(DictionaryMod_PPQ::ticking);
+		//ServerMessageEvents.ALLOW_CHAT_MESSAGE.register(MultiplicationGame::onMsgSent);
+		//ServerTickEvents.START_SERVER_TICK.register(MultiplicationGame::ticking);
 	}
 }
